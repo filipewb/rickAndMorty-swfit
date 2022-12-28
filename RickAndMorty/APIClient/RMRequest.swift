818e96fa975cb61091fc74969d90230d941cc95a
personal <filipe.boeck@hotmail.com>
2022-12-28 20:47:38 -0300

    adicionado documentacao e arquivos que serao usados na api

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
--- RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+/// Controller to show and search for Episodes
 final class RMEpisodeViewController: UIViewController {
 
     override func viewDidLoad() {
